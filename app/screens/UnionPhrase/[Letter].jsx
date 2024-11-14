5b5522da105122bfa33eebca49e3e8cb57a85629
Olstertecn11 <otzunund@miumg.edu.gt>
2024-11-13 19:25:11 -0600

    lecciones realizadas

--- app/screens/UnionPhrase/[Letter].jsx
+++ app/screens/UnionPhrase/[Letter].jsx
@@ -8,6 +8,7 @@ import { useRouter, useLocalSearchParams } from 'expo-router';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import unionPhrases from '../../../assets/data/union_phrases.json';
 import { Confetti } from 'react-native-fast-confetti';
+import { Audio } from 'expo-av';  // Importamos el módulo Audio de expo-av
 
 
 
@@ -23,6 +24,30 @@ const UnionPhrase = () => {
   const [isCorrect, setIsCorrect] = React.useState(false);
 
 
+
+  const [sound, setSound] = React.useState();
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const [isWrong, setIsWrong] = React.useState(false);
+
+  const playAudio = async (item) => {
+    try {
+      const { sound } = await Audio.Sound.createAsync(item);
+      setSound(sound);
+      await sound.playAsync();
+      setIsPlaying(true);
+    } catch (error) {
+      console.error("Error al reproducir el sonido:", error);
+    }
+  };
+
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+        sound.unloadAsync();
+      }
+      : undefined;
+  }, [sound]);
 
 
   const getRandomElements = (array, count) => {
@@ -51,7 +76,18 @@ const UnionPhrase = () => {
 
   React.useEffect(() => {
     if (answers.join('') === phrases[phraseIndex].join('')) {
+      playAudio(require("../../../assets/audio/letters/instructions/correcta.m4a"));
       setIsCorrect(true);
+      if (phraseIndex === phrases.length - 1) {
+        playAudio(require("../../../assets/audio/letters/instructions/felicidades.m4a"));
+      }
+    }
+    else {
+      if (answers.length > 0 && answers.length === phrases[phraseIndex].length) {
+        playAudio(require("../../../assets/audio/letters/instructions/incorrecta.m4a"));
+        setIsCorrect(false);
+        setIsWrong(true);
+      }
     }
   }, [answers]);
 
@@ -82,7 +118,7 @@ const UnionPhrase = () => {
       {isCorrect && <Confetti />}
       <Center mt={40} mb={70} px={6}>
         <Text fontSize='40px' fontWeight='bold' textAlign='center' color={Colors.buttonActive}>{phrases[phraseIndex].join(' ')}</Text>
-        <Box h={'50px'} w='100%' bg={!isCorrect ? Colors.buttonInactive : 'green.500'} borderRadius={8}>
+        <Box h={'50px'} w='100%' bg={!isCorrect ? isWrong ? 'red.600' : Colors.buttonInactive : 'green.500'} borderRadius={8}>
           <HStack space={1} alignItems='center' justifyContent='center' pt={2}>
             {
               answers.map((answer, index) => (
@@ -95,7 +131,6 @@ const UnionPhrase = () => {
         </Box>
         <HStack flexWrap={'wrap'} space={4} mx='10%' alignItems='center' justifyItems='center' justifyContent='center' mt={8}>
           {
-            // Render renderOptions without press method
             renderOptions.map((option, index) => (
               <Button key={index} bg={Colors.buttonInactive} mt={4} onPress={() => addAnswer(option)}>
                 <Text fontSize='16px' fontWeight='bold' color={Colors.buttonActive}>{option}</Text>
